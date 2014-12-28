4d725cb8c59b4653946cc556a7402f8fc4a44a5e
Ruei-Min Lin <rueiminl@andrew.cmu.edu>
2014-12-27 21:33:07 -0500

    fix minor changes for pet name
    Change-Id: I033db2f0dbb3e01dae0f5d5b34dd2f0560b16be8

--- ItemList.cpp
+++ ItemList.cpp
@@ -5,8 +5,10 @@
 	}
 	cout << endl;
 	cout << "==Number==" << endl;
+	cout << "<div class=\"mw-collapsible mw-collapsed\">" << endl;
 
-	ifstream input("ItemList.txt");
+
+	ifstream input("input.txt");
 	string last_line;
 	while (!input.eof())
 	{
@@ -21,18 +23,35 @@
 		if (!last_line.empty() && last_line[0] != line[0])
 		{
 			if (line[0] >= 'A' && line[0] <= 'Z')
+			{
+				cout << "</div>" << endl;
 				cout << "==" << line[0] << "==" << endl;
+				cout << "<div class=\"mw-collapsible mw-collapsed\">" << endl;
+				
+			}
 		}
 
 		last_line = line;
 
-		auto n = line.find("(");
+		string original_name = line;
+		auto n = original_name.find("(");
 		if (n != string::npos)
 		{
-			string link = line;
 			string name = line.substr(0, n-1);
 			string rank = line.substr(n+1, line.length()-n-2);
-			line = link + "|<nowiki>" + name + " [" + rank + "]</nowiki>";
+			line = original_name + "|<nowiki>" + name + " [" + rank + "]</nowiki>";
 		}
-		cout << "*[[" << line << "]]" << endl;
+
+		n = original_name.find("+");
+		if (n != string::npos)
+		{
+			string level = original_name.substr(n+1);
+			original_name = original_name.substr(0, n-1);
+			if (original_name.find("Keyblade") != string::npos)
+			{
+				original_name += " " + level;
+			}
+		}
+		cout << "*[[Image:" << original_name << ".gif]][[" << line << "]]" << endl;
 	}
+	cout << "</div>" << endl;
